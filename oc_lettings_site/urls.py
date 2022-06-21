d7ea541b2663092813b47e70459eb39010adc734
Jheslian <justinjhes@gmail.com>
2022-06-21 23:10:48 +0200

    merge with oc_lettings_site app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from . import views
+from django.views.generic import TemplateView
 
 app_name = 'oc_site'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('lettings/', include('lettings.urls')),
