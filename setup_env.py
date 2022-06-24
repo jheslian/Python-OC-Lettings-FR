9843c869d59b8a447f37a99bb626a1c203b8e4ba
Jheslian <justinjhes@gmail.com>
2022-06-24 20:19:52 +0200

    test env variables

--- setup_env.py
+++ setup_env.py
@@ -1,5 +1,5 @@
 from django.core.management.utils import get_random_secret_key
 
 with open(".env", "w") as f:
-    f.write(f"SECRET_KEY={get_random_secret_key()}\n")
+    f.write(f"export SECRET_KEY={get_random_secret_key()}\n")
     f.close()
