29924e9efb89a7b8919441d0d199c41098e74d23
Jheslian <justinjhes@gmail.com>
2022-06-26 20:31:21 +0200

    refactor admin of oc_lettings_site

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from lettings.models import Letting, Address
-from profiles.models import Profile
-
 
 admin.site.register(Letting)
 admin.site.register(Address)
-admin.site.register(Profile)
